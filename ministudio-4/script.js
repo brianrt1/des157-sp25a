e651f6c1e8838e469a08033f4224c2ea17820dee
Brian Rodriguez-torres <brodrigueztorres@ucdavis.edu>
2025-05-05 17:24:05 -0700

    push29

--- ministudio-4/script.js
+++ ministudio-4/script.js
@@ -1,2 +1,8 @@
 var scene = document.getElementById('scene');
 var parallaxInstance = new Parallax(scene);
+
+DeviceOrientationEvent
+  .requestPermission()
+  .then(() => {
+    new Parallax(scene)
+  })
