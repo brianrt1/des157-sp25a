f8b58b3a5eddc69cc89ea4e0d8cfec72778b0d82
Brian Rodriguez-torres <brodrigueztorres@ucdavis.edu>
2025-05-05 19:12:24 -0700

    push30

--- ministudio-4/script.js
+++ ministudio-4/script.js
@@ -1,8 +1,58 @@
 var scene = document.getElementById('scene');
 var parallaxInstance = new Parallax(scene);
 
-DeviceOrientationEvent
-  .requestPermission()
-  .then(() => {
-    new Parallax(scene)
-  })
+$(document).ready(function() {
+  // When hovering over overlay-1
+  $('.overlay-1').hover(
+    function() { // Mouse enter
+      $('.guitar-1').css('display', 'block').animate({opacity: 1}, 500); // Show and fade in
+      $('.comb-1').css('display', 'block').animate({opacity: 1}, 500);
+    },
+    function() { // Mouse leave
+      $('.guitar-1').animate({opacity: 0}, 500, function() {
+        $(this).css('display', 'none'); // Hide after fade out completes
+      });
+      $('.comb-1').animate({opacity: 0}, 500, function() {
+        $(this).css('display', 'none');
+      });
+    }
+  );
+
+  //
+
+  $('.overlay-2').hover(
+    function() { // Mouse enter 
+      $('.birds-1').css('display', 'block').animate({opacity: 1}, 500); // Show and fade in
+     
+    },
+    function() { // Mouse leave
+      
+      $('.birds-1').animate({opacity: 0}, 500, function() {
+        $(this).css('display', 'none'); // Hide after fade out completes
+      });
+      
+    }
+  );
+
+  $('.overlay-3').hover(
+    function() { // Mouse enter
+    
+      $('.tractor-1').css('display', 'block').animate({opacity: 1}, 500); // Show and fade in
+      $('.pitchfork-1').css('display', 'block').animate({opacity: 1}, 500);
+    },
+    function() { // Mouse leave
+      
+      $('.tractor-1').animate({opacity: 0}, 500, function() {
+        $(this).css('display', 'none'); // Hide after fade out completes
+      });
+      $('.pitchfork-1').animate({opacity: 0}, 500, function() {
+        $(this).css('display', 'none');
+      });
+    }
+  );
+
+  
+
+
+});
+
